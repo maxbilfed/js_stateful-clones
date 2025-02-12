aa0ef991c56861510c90dd7255fd653a8f2febac
Max Shapes <makc.bilous04@gmail.com>
2025-02-12 17:20:14 +0200

    made solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const newState = { ...state };
+  const clonedStates = [];
+
+  for (const elem of actions) {
+    if (elem.type === 'addProperties') {
+      Object.assign(newState, elem.extraData);
+      clonedStates.push({ ...newState });
+    } else if (elem.type === 'removeProperties') {
+      for (let i = 0; i < elem.keysToRemove.length; i++) {
+        delete newState[elem.keysToRemove[i]];
+      }
+      clonedStates.push({ ...newState });
+    } else {
+      for (const key in newState) {
+        delete newState[key];
+      }
+      clonedStates.push({ ...newState });
+    }
+  }
+
+  return clonedStates;
 }
 
 module.exports = transformStateWithClones;
