99925db6bb79bb1d6b86c1d1e58dc2cf5c65c22c
Max Shapes <makc.bilous04@gmail.com>
2025-02-13 16:41:09 +0200

    rewrote to switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,24 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-  const newState = { ...state };
+  const copyState = { ...state };
   const clonedStates = [];
 
   for (const elem of actions) {
-    if (elem.type === 'addProperties') {
-      Object.assign(newState, elem.extraData);
-      clonedStates.push({ ...newState });
-    } else if (elem.type === 'removeProperties') {
-      for (let i = 0; i < elem.keysToRemove.length; i++) {
-        delete newState[elem.keysToRemove[i]];
-      }
-      clonedStates.push({ ...newState });
-    } else {
-      for (const key in newState) {
-        delete newState[key];
-      }
-      clonedStates.push({ ...newState });
+    switch (elem.type) {
+      case 'addProperties':
+        Object.assign(copyState, elem.extraData);
+        break;
+
+      case 'removeProperties':
+        for (let i = 0; i < elem.keysToRemove.length; i++) {
+          delete copyState[elem.keysToRemove[i]];
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+        break;
+
+      default:
+        break;
     }
+
+    clonedStates.push({ ...copyState });
   }
 
   return clonedStates;
